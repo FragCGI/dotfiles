a8965a24f77a585c44607d232826144c2ea367d9
FragCGI <89751683+FragCGI@users.noreply.github.com>
2023-05-18 00:54:11 -0300

    Update waybar-wttr.py

--- waybar/custom/waybar-wttr.py
+++ waybar/custom/waybar-wttr.py
@@ -58,7 +58,7 @@ WEATHER_CODES = {
 data = {}
 
 
-weather = requests.get("https://wttr.in/Debert?format=j1").json()
+weather = requests.get("https://wttr.in/City?format=j1").json()
 
 
 def format_time(time):
